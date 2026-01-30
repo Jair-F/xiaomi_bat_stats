f696acb02b19079609ac8be7eaa4bc62181e447b
Jair-F <jair.fehlauer@gmail.com>
2026-01-30 17:20:19 +0200

    initial commit

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#define wifi_ssid "ssid"
+#define wifi_password "password"
+#define mqtt_server "192.168.1.1"
+#define mqtt_port 1883
+
+#define batteryTxPin D1
+#define batteryRxPin D2
+
+#define mqttTopic "wifi2mqtt/%s"
+#define mqttClientId "ninebattery-mqtt"
+#define mqttEnabled true
---
+++ readme.md
@@ -0,0 +1,7 @@
+#### Use ninebot ES1/2 battery as powerbank.
+
+esp8266 reads battery state and reports it through mqtt.
+
+TODO: implement charging control with relay to keep battery charged no more than 80% 
+
+big thanks to https://github.com/etransport/ninebot-docs/wiki/protocol
